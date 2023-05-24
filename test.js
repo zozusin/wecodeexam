56371b9eebaa2c1f28009ff225600e57f1e42efc
zosuzin <zosuzin@gmail.com>
2023-05-25 08:46:44 +0900

    짝수구하는 함수

---
+++ test.js
@@ -0,0 +1,11 @@
+const numberfun = () => {
+  let numberBox = [];
+  for (let i = 1; i < 51; i++) {
+    if (i % 2 === 0) {
+      numberBox.push(i);
+    }
+  }
+  return numberBox;
+};
+
+console.log(numberfun());
